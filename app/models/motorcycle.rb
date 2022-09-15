370746bf536947fdb19fec99d12fedce0e089cfe
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-15 14:56:47 +0200

    Add motor cycle model validation

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -2,4 +2,7 @@ class Motorcycle < ApplicationRecord
   belongs_to :category
   has_many :reservations
   has_many :users, through: :reservations
+
+  validates :brand, :model, :year, :image, presence: true
+  validates :rental_price, presence: true, numericality: { greater_than: 0 }
 end

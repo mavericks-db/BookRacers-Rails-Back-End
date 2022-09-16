5e807f5b1357c49392ffadbb6dc6eaa6640e6ce2
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-16 10:01:53 +0500

    Seed cat9 motorcycles

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,20 @@ require File.expand_path('db/seeds/seeds_user', Rails.root)
 p "Creating categories"
 require File.expand_path('db/seeds/seeds_category', Rails.root)
 
+p "Creating motorcycles"
+require File.expand_path('db/seeds/seeds_sports', Rails.root)
+require File.expand_path('db/seeds/seeds_scooters', Rails.root)
+require File.expand_path('db/seeds/seeds_nakedbikes', Rails.root)
+require File.expand_path('db/seeds/seeds_customcruisers', Rails.root)
+require File.expand_path('db/seeds/seeds_crossmotocross', Rails.root)
+require File.expand_path('db/seeds/seeds_supermotard', Rails.root)
+require File.expand_path('db/seeds/seeds_minibikesport', Rails.root)
+require File.expand_path('db/seeds/seeds_minibikecross', Rails.root)
+require File.expand_path('db/seeds/seeds_classics', Rails.root)
+
 p "------------------"
 
 p "Created #{User.count} users"
+p "Created #{Category.count} categories"
+p "Created #{Motorcycle.count} motorcycles"
 p "Seed finished"

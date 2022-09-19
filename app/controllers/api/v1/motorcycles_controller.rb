7bb212bf1ea91f3cbcb01935815fcff48c9bd27b
[Dinma-Faith] <[phaytep@gmail.com]>
2022-09-19 16:26:58 +0100

    Refactor index and show method in motorcycles_controller

--- app/controllers/api/v1/motorcycles_controller.rb
+++ app/controllers/api/v1/motorcycles_controller.rb
@@ -1,11 +1,22 @@
 class Api::V1::MotorcyclesController < ApplicationController
   def index
     @motorcycles = Motorcycle.all
-    render json: @motorcycles
+    if @motorcycles
+      render json: {motorcycle: @motorcycles}
+    else
+      render json: {error: 'No motorcycles yet'}
+    end
   end
 
   def show
     @motorcycle = Motorcycle.find(params[:id])
     render json: @motorcycle
+    if @motorcycle
+      render json: { motorcycle: @motorcycle }
+    else
+      render json: { error: 'Unable to find motorcycle' }
+    end
   end
+
+  
 end

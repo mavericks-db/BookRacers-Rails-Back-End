8b1d50ed46f3ef3a6e530a6e826c3d873c6e539a
collinsmezie <collinschimezieobinwa@gmail.com>
2022-09-19 14:35:10 +0400

    add method to create a reservation

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -7,6 +7,24 @@ class Api::V1::ReservationsController < ApplicationController
     def show
         @reserved_motorcycles = Reservation.where(user_id: params[:user_id])
         render json: @reserved_motorcycles
+    end
+
+    def create
+        @motorcycle = Motorcycle.find(params[:id])
+        @reservation = Reservation.new(reservation_params)
+        @reservation.motorcycle_id = @motorcycle.id
+        @reservation.user_id = current_user.id
+        @reservation.total_price =
+      if  @reservation.save
+          render json: status: :success
+      else
+        render json: status: :unsuccessful
       end
+    end
+
+    private
+    def reservation_params
+      params.require(:reservation).permit(:start_date, :end_date, :city)
+    end
 
   end

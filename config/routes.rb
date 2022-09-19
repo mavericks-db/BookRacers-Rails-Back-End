6b39916b8a6c75f2ad1eaaabaa38c250a45fe07f
[Dinma-Faith] <[phaytep@gmail.com]>
2022-09-19 16:33:58 +0100

    Add file routes to config routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,14 @@ Rails.application.routes.draw do
       get '/reservations', to: 'reservations#index'
       get '/reservations/:id', to: 'reservations#show'
       post '/add_reservation', to: 'reservations#create'
+      get '/motorcycles', to: 'motorcycles#index'
+      get '/motorcycles/:id', to: 'motorcycles#show'
+      post '/add_motorcycle', to: 'motorcycles#create'
+      delete '/motorcycles/:id', to: 'motorcycles#destroy'
+      get '/categories', to: 'categories#index'
+      get '/categories/:id', to: 'categories#show'
+      post '/add_category', to: 'categories#create'
+      delete '/categories/:id', to: 'categories#destroy'
     end
   end
 

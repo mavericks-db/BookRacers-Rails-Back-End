85372754dc81cd8e834f9834f569f9872c493f47
collinsmezie <collinschimezieobinwa@gmail.com>
2022-09-19 14:33:22 +0400

    add method to fetch a reservation

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::ReservationsController < ApplicationController
       render json: @reservations
     end
 
+    def show
+        @reserved_motorcycles = Reservation.where(user_id: params[:user_id])
+        render json: @reserved_motorcycles
+      end
+
   end

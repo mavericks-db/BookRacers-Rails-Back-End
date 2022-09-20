c3e57b069582b7c7956a5f06e32802fb16bd9917
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-20 12:56:13 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_050029) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_073635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_050029) do
     t.string "catname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "motorcycles", force: :cascade do |t|

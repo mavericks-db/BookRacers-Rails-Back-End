4e967aea8281164e5e96d62219dad38b8b11e296
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-20 12:49:58 +0200

    Refactor motocycles controller

--- app/controllers/api/v1/motorcycles_controller.rb
+++ app/controllers/api/v1/motorcycles_controller.rb
@@ -1,18 +1,17 @@
 class Api::V1::MotorcyclesController < ApplicationController
   def index
-    @motorcycles = Motorcycle.all
-    if @motorcycles
-      render json: { motorcycle: @motorcycles }
+    motorcycles = Motorcycle.order(created_at: :desc).includes([:reservations])
+    if motorcycles
+      render json: motorcycles
     else
       render json: { error: 'No motorcycles yet' }
     end
   end
 
   def show
-    @motorcycle = Motorcycle.find(params[:id])
-    render json: @motorcycle
-    if @motorcycle
-      render json: { motorcycle: @motorcycle }
+    motorcycle = Motorcycle.find_by_id(params[:id])
+    if motorcycle
+      render json: motorcycle
     else
       render json: { error: 'Unable to find motorcycle' }
     end

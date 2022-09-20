ca9b7a1d6bcae46dfb481e8ba47c2c440b168efd
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-09-20 12:53:53 +0200

    Add remove redundant routes from routes files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :categories
-      resources :motorcycles
       post '/signup', to: 'users#create'
       post '/login', to: 'sessions#login'
       get '/logout', to: 'sessions#destroy'

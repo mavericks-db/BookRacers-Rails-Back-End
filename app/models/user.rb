543f5f77239e8f1f071afdfbd687433195cda55b
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-23 11:29:59 +0200

    Arrange 500 error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,4 @@ class User < ApplicationRecord
 
   validates :name, :password, presence: true
   validates :email, presence: true, uniqueness: true
-
-  alias_attribute :password_digest, :encrypted_password
 end

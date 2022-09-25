11bfd090dc8e4f03b5a8eb1f04ea2d64452e4e8a
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-09-25 21:14:22 +0500

    Change login requirements to only username

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,9 +2,8 @@ class Api::V1::SessionsController < ApplicationController
   before_action :logged_in, only: [:show]
 
   def create
-    user = User.find_by_email(user_params[:email])
-
-    if user&.authenticate(user_params[:password])
+    user = User.find_by_username(user_params[:username])
+    if user
       token = issue_token(user)
       render json: { user: UserSerializer.new(user), jwt: token }
     else
@@ -23,6 +22,6 @@ class Api::V1::SessionsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:username)
   end
 end

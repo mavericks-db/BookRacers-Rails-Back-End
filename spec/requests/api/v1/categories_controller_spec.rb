d8c515ccec4eb08d20d08a659dd0a9e1e6498367
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-28 21:31:39 +0200

    Add requests spec for #index for categories

--- spec/requests/api/v1/categories_controller_spec.rb
+++ spec/requests/api/v1/categories_controller_spec.rb
@@ -1,44 +1,53 @@
-class Api::V1::CategoriesController < ApplicationController
-  before_action :logged_in
+require 'rails_helper'
 
-  def index
-    @categories = Category.includes(%i[motorcycles picture_attachment]).order(created_at: :desc)
-    if @categories
-      render json: @categories
-    else
-      render json: { error: 'No categories yet' }
+RSpec.describe Api::V1::CategoriesController, type: :request do
+  describe 'GET #index' do
+    context 'User is not authenticated' do
+      before :each do
+        get api_v1_categories_path
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'assigns all categories to @categories' do
+        expect(assigns(:categories)).to_not eq(Category.all)
+      end
+
+      it 'gives an error message' do
+        expect(response.body).to eq "{\"error\":\"You are not logged in\"}"
+      end
     end
-  end
 
-  def show
-    @category = Category.where(id: params[:id])
-    if @category
-      render json: @category
-    else
-      render json: { error: 'Motorcycle not found' }
-    end
-  end
+    # context 'User is authenticated' do
+    #   before :each do
+    #     headers = { "ACCEPT" => "application/json" }
+    #     post api_v1_signup_path, params: {
+    #       user: {
+    #         name: 'John Doe',
+    #         username: 'johndoe',
+    #         email: 'johndoe@mail.com',
+    #         password: '123456',
+    #         admin: false
+    #       }
+    #     }, headers: headers
+    #     # follow_redirect!
+    #     get api_v1_authorized_path
+    #     get api_v1_categories_path
+    #   end
 
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-      render json: { message: @category }, status: :ok
-    else
-      render json: { error: 'Error creating category' }
-    end
-  end
+    #   it 'returns http success' do
+    #     expect(response).to have_http_status(:ok)
+    #   end
 
-  def destroy
-    if Category.find(params[:id]).destroy
-      render json: { message: 'Category deleted' }
-    else
-      render json: { error: 'Error deleting category' }
-    end
-  end
+    #   it 'redirects to the home page' do
+    #     expect(response).to redirect_to api_v1_categories_path
+    #   end
 
-  private
-
-  def category_params
-    params.require(:category).permit(:catname, :image, :picture)
+    #   it 'assigns all categories to @categories' do
+    #     expect(assigns(:categories)).to eq(Category.all)
+    #   end
+    # end
   end
 end

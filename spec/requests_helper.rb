be158a7ef19d784aff1fd290b30a85ba37d5488d
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-28 21:31:55 +0200

    Add a spec helper method

---
+++ spec/requests_helper.rb
@@ -0,0 +1,13 @@
+def login
+  headers = { "ACCEPT" => "application/json" }
+  post api_v1_signup_path, params: {
+    user: {
+      name: 'John Doe',
+      username: 'johndoe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+      admin: false
+    }
+  }, headers: headers
+  follow_redirect!
+end

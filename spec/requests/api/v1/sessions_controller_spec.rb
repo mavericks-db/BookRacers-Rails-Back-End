02cc8cf908fa50cc7873e46affc1001be5368d23
Jonathan Sivahera <jsivahera@icloud.com>
2022-09-29 01:36:24 +0200

    Add user logout spec

--- spec/requests/api/v1/sessions_controller_spec.rb
+++ spec/requests/api/v1/sessions_controller_spec.rb
@@ -12,44 +12,14 @@ RSpec.describe Api::V1::SessionsController, type: :request do
       end
     end
 
-    # context 'checks authorization' do
-    #   before :each do
-    #     auth_token = login
-    #     headers = { "ACCEPT" => "application/json", "Authorization" => auth_token }
-    #     get api_v1_users_path, headers: headers
-    #   end
+    context 'checks logout' do
+      before :each do
+        get api_v1_logout_path
+      end
 
-    #   it 'returns http success' do
-    #     expect(response).to have_http_status(:ok)
-    #   end
-
-    #   it 'redirects to the home page' do
-    #     expect(current_path).to eq(api_v1_users_path)
-    #   end
-
-    #   it 'assigns all categories to @categories' do
-    #     expect(@categories).to eq(Category.all)
-    #   end
-    # end
-
-    # context 'checks logout' do
-    #   before :each do
-    #     auth_token = login
-    #     headers = { "ACCEPT" => "application/json", "Authorization" => auth_token }
-    #     get api_v1_users_path, headers: headers
-    #   end
-
-    #   it 'returns http success' do
-    #     expect(response).to have_http_status(:ok)
-    #   end
-
-    #   it 'redirects to the home page' do
-    #     expect(current_path).to eq(api_v1_users_path)
-    #   end
-
-    #   it 'assigns all categories to @categories' do
-    #     expect(@categories).to eq(Category.all)
-    #   end
-    # end
+      it 'returns http success' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
   end
 end

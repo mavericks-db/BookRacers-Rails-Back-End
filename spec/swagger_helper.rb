5bfb760de530dc5eee6d47503c1fbb39ea93e619
collinsmezie <collinschimezieobinwa@gmail.com>
2022-09-29 19:40:04 +0400

    update database

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,19 +1,7 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.configure do |config|
-  # Specify a root folder where Swagger JSON files are generated
-  # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
-  # to ensure that it's configured to serve Swagger from the same folder
+RSpec.configure do |config| # rubocop:todo Metrics/BlockLength
   config.swagger_root = Rails.root.join('swagger').to_s
-
-  # Define one or more Swagger documents and provide global metadata for each one
-  # When you run the 'rswag:specs:swaggerize' rake task, the complete Swagger will
-  # be generated at the provided relative path under swagger_root
-  # By default, the operations defined in spec files are added to the first
-  # document below. You can override this behavior by adding a swagger_doc tag to the
-  # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
     'v1/swagger.yaml' => {
       openapi: '3.0.1',
@@ -23,23 +11,23 @@ RSpec.configure do |config|
       },
       paths: {},
       tags: [
-            {
-                name: 'Users',
-                description: 'User API endpoint requests'
-            },
-            {
-                name: 'Categories',
-                description: 'Category API endpoint requests'
-            },
-           {
-                name: 'Motorcycles',
-                description: 'Motorcycle API endpoint requests'
-            },
-           {
-                name: 'Reservations',
-                description: 'Reservation API endpoint requests'
-            },
-           ],
+        {
+          name: 'Users',
+          description: 'User API endpoint requests'
+        },
+        {
+          name: 'Categories',
+          description: 'Category API endpoint requests'
+        },
+        {
+          name: 'Motorcycles',
+          description: 'Motorcycle API endpoint requests'
+        },
+        {
+          name: 'Reservations',
+          description: 'Reservation API endpoint requests'
+        }
+      ],
       servers: [
         {
           url: 'https://{defaultHost}',

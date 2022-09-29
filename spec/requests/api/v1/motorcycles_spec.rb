7a1ae7dad58bd3704bd7a76dadb94f13a20c3c90
collinsmezie <collinschimezieobinwa@gmail.com>
2022-09-29 16:52:42 +0400

    generate test for motorcycle endpoints

---
+++ spec/requests/api/v1/motorcycles_spec.rb
@@ -0,0 +1,97 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/motorcycles', type: :request do
+
+  path '/api/v1/motorcycles' do
+
+    get('list motorcycles') do
+      tags 'Motorcycles'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/motorcycles/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show motorcycle') do
+      tags 'Motorcycles'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/add_motorcycle' do
+
+    post('create motorcycle') do
+      tags 'Motorcycles'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/del_motorcycle' do
+
+    delete('delete motorcycle') do
+      tags 'Motorcycles'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/update_motorcycle' do
+
+    patch('update motorcycle') do
+      tags 'Motorcycles'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end

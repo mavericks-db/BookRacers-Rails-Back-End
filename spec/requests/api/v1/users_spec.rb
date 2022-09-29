912c37db55e623e84fc5ed093bad8b284cdf3e25
collinsmezie <collinschimezieobinwa@gmail.com>
2022-09-29 18:43:28 +0400

    Add parameters to sign in and user endpoints

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,13 +1,9 @@
 require 'swagger_helper'
-
 RSpec.describe 'api/v1/users', type: :request do
-
   path '/api/v1/users' do
-
     get('list users') do
       tags 'Users'
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -21,10 +17,21 @@ RSpec.describe 'api/v1/users', type: :request do
   end
 
   path '/api/v1/signup' do
-
     post('create user') do
       tags 'Users'
       response(200, 'successful') do
+        consumes 'application/json'
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            username: { type: :string },
+            email: { type: :string },
+            password: { type: :string },
+            admin: { type: :boolean }
+          },
+          required: %w[name username email password admin]
+        }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -39,11 +46,9 @@ RSpec.describe 'api/v1/users', type: :request do
   end
 
   path '/api/v1/login' do
-
     post('login user') do
       tags 'Users'
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
